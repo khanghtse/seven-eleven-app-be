89fe0a2b5ec237bacb5b3f118f78abe7ce6762e3
khanghtse <khanghoang090703@gmail.com>
2026-05-14 22:00:07 +0700

    cors fix

--- src/main/java/com/seveneleven/backend/config/CorsConfig.java
+++ src/main/java/com/seveneleven/backend/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+                .allowedOrigins("http://localhost:5173", "https://seven-eleven-app-fe.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
